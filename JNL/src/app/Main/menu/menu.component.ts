3c00d1adae90572357c1dfa126e36699694fe184
Ovidiu Cosma <OvidiuCosma@jnl.be>
2018-12-28 13:20:18 +0200

    menu comp toggle

--- JNL/src/app/Main/menu/menu.component.ts
+++ JNL/src/app/Main/menu/menu.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnChanges {
+
+  @Input() menuBarOpen = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnChanges() {
+    this.toggleMenuBar();
+  }
+
+  toggleMenuBar() {
+    this.menuBarOpen = !this.menuBarOpen;
+  }
 }
