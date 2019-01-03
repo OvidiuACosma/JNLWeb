665fdb3ad6604f9593ea63fa9ada07a0b45e2810
Ovidiu Cosma <OvidiuCosma@jnl.be>
2019-01-04 00:59:57 +0200

    Ovi: menu links solved

--- JNL/src/app/Std/marques/marques.component.ts
+++ JNL/src/app/Std/marques/marques.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-marques',
   templateUrl: './marques.component.html',
   styleUrls: ['./marques.component.css']
 })
-export class MarquesComponent implements OnInit {
+export class MarquesComponent implements OnInit, AfterViewChecked {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
 
+  ngAfterViewChecked() {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        this.navigateToAnchor(fragment);
+      } else {
+          window.scrollTo(0, 0);
+        }
+    });
+  }
+
+  navigateToAnchor(fragment: string) {
+    const element = document.getElementById(fragment);
+    if (element) {
+      element.scrollIntoView({block: 'start', behavior: 'smooth'});
+    }
+  }
 }
