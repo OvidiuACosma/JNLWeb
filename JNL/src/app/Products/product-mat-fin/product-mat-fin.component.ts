ab61bb0ffc9056c6e0a9f618c60f88f3434ceb39
Tunde Han <TundeHan@jnl.be>
2019-01-08 16:41:41 +0200

    Tunde

--- JNL/src/app/Products/product-mat-fin/product-mat-fin.component.ts
+++ JNL/src/app/Products/product-mat-fin/product-mat-fin.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductMatFinComponent implements OnInit {
 
+  public detail = 'tissus';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setDetail(index: number) {
+    switch (index) {
+      case 0: {
+        this.detail = 'tissu';
+        break;
+      }
+      case 1: {
+        this.detail = 'cuir';
+        break;
+      }
+      case 2 : {
+        this.detail = 'metal';
+        break;
+      }
+      case 3 : {
+        this.detail = 'bois';
+        break;
+      }
+      case 4 : {
+        this.detail = 'verre';
+        break;
+      }
+      case 5 : {
+        this.detail = 'miroir';
+        break;
+      }
+    }
   }
 
 }
