1d6f0cc2229c38bfbc46f8d5ef7cd7e44b72ee79
Ovidiu Cosma <OvidiuCosma@jnl.be>
2019-01-13 18:33:14 +0200

    ovi: adj press

--- JNL/src/app/Std/press/press.component.ts
+++ JNL/src/app/Std/press/press.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-press',
   templateUrl: './press.component.html',
   styleUrls: ['./press.component.css']
 })
-export class PressComponent implements OnInit {
+export class PressComponent implements OnInit, AfterViewChecked {
 
-  constructor() { }
+  public section: string;
+
+  constructor(private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.section = params['section'];
+    });
+  }
+
+  ngAfterViewChecked() {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        this.navigateToAnchor(fragment);
+      } else {
+          window.scrollTo(0, 0);
+        }
+    });
+  }
+
+  navigateToAnchor(fragment: string) {
+    const element = document.getElementById(fragment);
+    if (element) {
+      element.scrollIntoView({block: 'start', behavior: 'smooth'});
+    }
+  }
+
+  navigateTo(target: string, fragment: string = '') {
+    if (fragment === '') {
+      this.router.navigate([target]);
+      this.ScrollTop();
+    } else {
+      this.router.navigate([target], {fragment: fragment});
+    }
+  }
+
+  ScrollTop() {
+    window.scrollTo(0, 0);
   }
 
 }
