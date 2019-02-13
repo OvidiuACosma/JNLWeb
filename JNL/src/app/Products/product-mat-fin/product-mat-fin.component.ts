d934980e2abd64b2795ea405729f91ae4f4654c8
TundeHan <TundeHan@jnl.be>
2019-02-13 16:31:33 +0200

    tunde

--- JNL/src/app/Products/product-mat-fin/product-mat-fin.component.ts
+++ JNL/src/app/Products/product-mat-fin/product-mat-fin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService, PagerService } from 'src/app/_services';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-product-mat-fin',
@@ -19,17 +20,6 @@ export class ProductMatFinComponent implements OnInit {
 
   // test tab visibility
   public cuirTab: boolean;
-
-  // pager object
-  pager: any = {};
-
-  // paged items
-  tissuPagedItems: any[];
-  cuirPagedItems: any[];
-  similiPagedItems: any[];
-
-  // page size (number of thumbnails) for pager
-  pageSize: number;
 
   // data used in the modal
   public imgSource = '';
@@ -52,8 +42,6 @@ export class ProductMatFinComponent implements OnInit {
     this.productsService.getTissus()
       .subscribe(tissus => {
         this.tissus = tissus;
-        // initialize to page 1
-        this.setPage(1);
       });
 
     // get Cuirs
@@ -67,77 +55,31 @@ export class ProductMatFinComponent implements OnInit {
       .subscribe(similiCuirs => {
         this.similiCuirs = similiCuirs;
       });
-
-    this.setPageSize();
   }
 
   setCategory(category: string) {
     switch (category) {
       case 'tissu': {
         this.matCategory = 'tissu';
-        this.setPage(1);
         break;
       }
       case 'cuir': {
         this.matCategory = 'cuir';
-        this.setPage(1);
         break;
       }
       case 'simili': {
         this.matCategory = 'simili';
-        this.setPage(1);
         break;
       }
       case 'abatjour': {
         this.matCategory = 'abatjour';
-        this.setPage(1);
         break;
       }
       case 'metal': {
         this.matCategory = 'metal';
-        this.setPage(1);
         break;
       }
     }
-  }
-
-  // set number of thumbnails per page
-  setPageSize() {
-    const cssWidth: number = window.innerWidth;
-    // xl
-    if (cssWidth >= 1200) {
-      this.pageSize = 24;
-      // lg
-    } else if (cssWidth < 1200 && cssWidth >= 992) {
-      this.pageSize = 16;
-      // md
-    } else if (cssWidth < 992 && cssWidth >= 768) {
-      this.pageSize = 12;
-      // sm
-    } else if (cssWidth < 768) {
-      this.pageSize = 8;
-    }
-  }
-
-  setPage(page: number) {
-    if (this.matCategory === 'tissu') {
-      // get pager object from service
-      this.pager = this.pagerService.getPager(this.tissus.length, page, this.pageSize);
-
-      // get current page of items
-      this.tissuPagedItems = this.tissus.slice(this.pager.startIndex, this.pager.endIndex + 1);
-    } else if (this.matCategory === 'cuir') {
-      this.pager = this.pagerService.getPager(this.cuirs.length, page, this.pageSize);
-      this.cuirPagedItems = this.cuirs.slice(this.pager.startIndex, this.pager.endIndex + 1);
-    } else if (this.matCategory === 'simili') {
-      this.pager = this.pagerService.getPager(this.similiCuirs.length, page, this.pageSize);
-      this.similiPagedItems = this.similiCuirs.slice(this.pager.startIndex, this.pager.endIndex + 1);
-    }
-  }
-
-  onResize() {
-    this.setPageSize();
-    this.setPage(1);
   }
 
   sendDataToModal(event) {
