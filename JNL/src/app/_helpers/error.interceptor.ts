5ff139ad9599061fd4e640bb75834cf0959f82f6
Ovidiu Cosma <OvidiuCosma@jnl.be>
2019-03-10 23:00:45 +0200

    ovi

--- JNL/src/app/_helpers/error.interceptor.ts
+++ JNL/src/app/_helpers/error.interceptor.ts
@@ -1,25 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, retry } from 'rxjs/operators';
 
 import { AuthenticationService } from '../_services';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) {}
+  constructor(private authenticationService: AuthenticationService) {}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request)
-        .pipe(catchError(err => {
-            if (err.status === 401) {
-                // auto logout if 401 response returned from api
-                this.authenticationService.logout();
-                location.reload(true);
-            }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request)
+    .pipe(retry(1),
+    catchError((err: HttpErrorResponse) => {
+      if (err.error instanceof ErrorEvent) {
+        // A client-side or network error occurred. Handle it accordingly.
+        console.error('An error occurred:', err.error.message);
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong
+        if (err.status === 401) {
+          // auto logout if 401 response returned from api
+          this.authenticationService.logout();
+          location.reload(true);
+        }}
 
-            const error = err.error.message || err.statusText;
-            return throwError(error);
-        }));
-    }
+        const error = err.error.message || err.statusText;
+        return throwError(error);
+      }));
+  }
 }
