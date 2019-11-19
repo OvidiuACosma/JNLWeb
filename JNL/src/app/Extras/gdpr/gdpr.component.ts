ef6ca866c3667bb4cdde8fffacf9411e9a7c2fdc
TundeHan <TundeHan@jnl.be>
2019-11-19 15:52:25 +0200

    gdpr

--- JNL/src/app/Extras/gdpr/gdpr.component.ts
+++ JNL/src/app/Extras/gdpr/gdpr.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataExchangeService } from 'src/app/_services';
 
 @Component({
   selector: 'app-gdpr',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GDPRComponent implements OnInit {
 
-  constructor() { }
+  language: string;
+
+  constructor(private dataex: DataExchangeService) { }
 
   ngOnInit() {
+    this.dataex.currentLanguage
+    .subscribe(lang => {
+      this.language = lang || 'EN';
+    });
   }
 
 }
